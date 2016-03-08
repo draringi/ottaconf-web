dc8ab77eccf517b11a310522a5cb5eee4e3a3fc6
Michael Williams <draringi@draringi.net>
2016-03-08 12:38:24 -0500

    Fix Schema

--- db/schema-v1.sql
+++ db/schema-v1.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS "conference" (
 	id SERIAL PRIMARY KEY,
-	name varchar(32) NOT NULL CHECK(name != ""),
+	name varchar(32) NOT NULL CHECK(name <> ''),
 	year int,
-	schedule_url varchar(128) NOT NULL CHECK(scheduleURL != ""),
-	person_base varchar(128) NOT NULL CHECK(scheduleURL != ""),
+	schedule_url varchar(128) NOT NULL CHECK(schedule_url <> ''),
+	person_base varchar(128) NOT NULL CHECK(person_base <> ''),
 	CONSTRAINT conference_year_uniqueness UNIQUE(name, year)
 );
 
 CREATE TABLE IF NOT EXISTS "schedule" (
 	conference int NOT NULL REFERENCES "conference"(id),
-	version varchar(32) NOT NULL CHECK(name != ""),
-	path varchar(128) NOT NULL CHECK(name != "") UNIQUE,
+	version varchar(32) NOT NULL CHECK(version <> ''),
+	xml_path varchar(128) NOT NULL CHECK(xml_path <> '') UNIQUE,
 	CONSTRAINT schedule_key PRIMARY KEY(conference, version)
 );
