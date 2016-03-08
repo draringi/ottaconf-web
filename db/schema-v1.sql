d249243ae9575cde0f8e0996f102b3f769dcfe81
Michael Williams <draringi@draringi.net>
2016-03-08 11:47:25 -0500

    Initialize db in travis

--- db/schema-v1.sql
+++ db/schema-v1.sql
@@ -1,6 +1,15 @@
 CREATE TABLE IF NOT EXISTS "conference" (
 	id SERIAL PRIMARY KEY,
-	name STRING NOT NULL CHECK(name != ""),
+	name varchar(32) NOT NULL CHECK(name != ""),
 	year int,
-	
+	schedule_url varchar(128) NOT NULL CHECK(scheduleURL != ""),
+	person_base varchar(128) NOT NULL CHECK(scheduleURL != ""),
+	CONSTRAINT conference_year_uniqueness UNIQUE(name, year)
+);
+
+CREATE TABLE IF NOT EXISTS "schedule" (
+	conference int NOT NULL REFERENCES "conference"(id),
+	version varchar(32) NOT NULL CHECK(name != ""),
+	path varchar(128) NOT NULL CHECK(name != "") UNIQUE,
+	CONSTRAINT schedule_key PRIMARY KEY(conference, version)
 );
